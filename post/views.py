d80e255e36cccd5432757282e21463b23a9af425
junhkchoi <leisure1566@gmail.com>
2025-03-20 23:55:32 +0900

    [Feat] home, post 완성

--- post/views.py
+++ post/views.py
@@ -5,7 +5,11 @@ from .models import Post
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'post/post_list.html')
+    post_list = Post.objects.all()
+    context = {
+        'post_list': post_list
+    }
+    return render(request, 'post/post_list.html', context)
 
 
 def create_post(request):
@@ -13,7 +17,8 @@ def create_post(request):
     if request.method == 'POST':
         post = Post.objects.create( #post 객체 생성
             title = request.POST.get('title'), # 폼에서 title 가져와서 title 변수에 저장
-            content = request.POST.get('content') 
+            content = request.POST.get('content'),
+            name = request.POST.get('name'),
         )
         return redirect(reverse('detail_post', args=[post.pk])) #detail_post에 pk를 같이 넘기면서 리다이렉트
     return render(request, 'post/create_post.html')
