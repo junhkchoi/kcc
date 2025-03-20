6520e188ed9926f039b6b5b7f005587a8e000468
junhkchoi <leisure1566@gmail.com>
2025-03-20 22:19:10 +0900

    [Feat] CRUD(C)

--- post/models.py
+++ post/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
 
-    title = models.CharField(max_length=20)
-    content = models.TextField()
+    title = models.CharField(max_length=20, blank=False, null=False)
+    content = models.TextField(blank=False, null=False)
     create_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
--- post/views.py
+++ post/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
+from .models import Post
 
 # Create your views here.
 def post_list(request):
     return render(request, 'post/post_list.html')
 
+
+def create_post(request):
+
+    if request.method == 'POST':
+        post = Post.objects.create( #post 객체 생성
+            title = request.POST.get('title'), # 폼에서 title 가져와서 title 변수에 저장
+            content = request.POST.get('content') 
+        )
+        return redirect(reverse('detail_post', args=[post.pk])) #detail_post에 pk를 같이 넘기면서 리다이렉트
+    return render(request, 'post/create_post.html')
